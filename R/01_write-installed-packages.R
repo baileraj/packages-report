9db515cda0ade6971d1ace655de6eaf6fb3343b8
John Bailer <baileraj@miamioh.edu>
2018-02-01 11:47:19 -0800

    updated with other necessary libraries and included write to construct the csv

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,8 @@
 ## deja vu from yesterday!
 
 library(dplyr)
+library(here)
+library(readr)
 
 ## create a data frame of your installed packages
 
@@ -22,6 +24,8 @@ myPackageDF <- allPackageDF %>%
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+
+write_csv(myPackageDF, here("data","install-packages.csv"))
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
